7a0154b7a50755147e237a2628553a521c9a583e
hitesh911 <hp354998@gmail.com>
2023-07-11 22:49:50 +0530

    added onboarding screens

--- lib/constants/assets_constants.dart
+++ lib/constants/assets_constants.dart
@@ -2,6 +2,12 @@ class AssetsConstants {
   static const String _pngPath = "assets/pngs";
   static const String _svgPath = "assets/svgs";
   static const String bird = "$_pngPath/bird.png";
-  static const String logo = "$_svgPath/logo.svg";
+  static const String buddy = "$_pngPath/Buddy.png";
+  static const String reserveVehicle = "$_pngPath/ReserveVehicle.png";
+  static const String scango = "$_pngPath/ScanGo.png";
+  static const String shareRide = "$_pngPath/ShareRide.png";
+  static const String supercharge = "$_pngPath/Supercharge.png";
+  static const String swapbattery = "$_pngPath/SwapBattery.png";
   static const String signUpHome = "$_svgPath/scanAndGo.svg";
+  static const String logo = "$_svgPath/logo.svg";
 }
